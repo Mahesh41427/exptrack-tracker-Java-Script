4c9f7721fa1d4574a0ac1cac2671445cddc64497
Mahesh41427 <149293130+Mahesh41427@users.noreply.github.com>
2024-01-30 13:56:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT
+const Expense = require('./models/expense');
+mongoose.connect('mongodb+srv://kishorek27:kishore27@cluster0.6slsaqa.mongodb.net/newDb?retryWrites=true&w=majority',{
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.get('/expenses', async(req, res) => {
+  const result=await Expense.find()
+  res.send(result)
+})
+
+
+app.post('/expenses', async(req, res) => {
+  console.log(req.body);
+  const newExpense = req.body;
+  await Expense.create(newExpense);
+  const result1=await Expense.find()
+  //res.send(result1)
+  res.send('Created');
+})
+
+// app.get('/expense/:id', async (req, res) => {
+//   try{
+//   const id = req.params.id;
+//   const result = await Expense.findById(id);
+//   if(result)
+//         res.send(result);
+//   else
+//       res.send(("No expense with that id"));
+//   }catch(err){
+//     res.send(err);
+//   }
+// })
+
+app.delete('/expense/:id', async (req, res) => {
+    try{
+    const id = req.params.id;
+    const result = await Expense.findByIdAndDelete(id);
+    if(result)
+          res.send(result);
+    else
+        res.send(("No expense with that id"));
+    }catch(err){
+      res.send(err);
+    }
+  })
+
+
+  app.put('/expense/:id',async(req,res) => {
+    const id = req.params.id;
+    const updateObject = req.body;
+    const updatedObject = await Expense.findByIdAndUpdate(id,{$set: updateObject},{
+      new: true
+    })
+    res.send(updatedObject)
+  
+  })
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
